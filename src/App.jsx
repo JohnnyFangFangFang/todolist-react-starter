429e85f902080a2ef5cd19cc7a01e792aea8291f
JohnnyFangFangFang <johnny781222@gmail.com>
2023-05-06 21:44:22 +0800

    Add Header props

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // 引入所需元件
 import { HomePage, LoginPage, SignUpPage, TodoPage } from 'pages';
 
+// 設定路由與對應渲染畫面
 function App() {
   return (
     <div className="app">
