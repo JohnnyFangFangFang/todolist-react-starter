b19f4a04c1c8eece3e120b029d621120eb8ae786
JohnnyFangFangFang <johnny781222@gmail.com>
2023-05-06 21:20:22 +0800

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.scss';
-import { TodoPage, LoginPage, SignUpPage } from './pages';
+// 載入路由的套件
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// 引入所需元件
+import { HomePage, LoginPage, SignUpPage, TodoPage } from 'pages';
 
 function App() {
   return (
     <div className="app">
-      <TodoPage />
+      <BrowserRouter>
+      <Routes>
+        <Route path='login' element={<LoginPage />} />
+        <Route path='signup' element={<SignUpPage />} />
+        <Route path='todo' element={<TodoPage />} />
+        <Route path='*' element={<HomePage />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
