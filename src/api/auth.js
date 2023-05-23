b8e417a8b85229359d9bde5f6ef5b6c22b85e4ad
JohnnyFangFangFang <johnny781222@gmail.com>
2023-05-23 20:54:01 +0800

    Add register api function and add it to signup page

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const authURL = 'https://todo-list.alphacamp.io/api/auth';
 
+// 登入
 export const login = async ({ username, password }) => {
   try {
     const { data } = await axios.post(`${authURL}/login`, {
@@ -16,5 +17,23 @@ export const login = async ({ username, password }) => {
     return data;
   } catch (error) {
     console.error('[Login Failed]:', error);
+  }
+};
+
+// 註冊
+export const register = async ({ username, email, password }) => {
+  try {
+    const { data } = await axios.post(`${authURL}/register`, {
+      username,
+      email,
+      password,
+    });
+    const { authToken } = data;
+    if (authToken) {
+      return { success: true, ...data };
+    }
+    return data;
+  } catch (error) {
+    console.error('[Register Failed]: ', error);
   }
 };
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -8,11 +8,55 @@ import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+// 引入彈跳視窗的套件
+import Swal from 'sweetalert2';
+import { register } from '../ap;i/auth';
 
 const SignUpPage = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  // 註冊按鈕
+  const handleClick = async () => {
+    if (username.length === 0) {
+      return;
+    }
+    if (password.length === 0) {
+      return;
+    }
+    if (email.length === 0) {
+      return;
+    }
+
+    const { success, authToken } = await register({
+      username,
+      email,
+      password,
+    });
+
+    if (success) {
+      localStorage.setItem('authToken', authToken);
+      // 彈出登入成功訊息
+      Swal.fire({
+        position: 'top',
+        title: '註冊成功！',
+        timer: 1000,
+        icon: 'success',
+        showConfirmButton: false,
+      });
+      return;
+    }
+    // 彈出登入失敗訊息
+    Swal.fire({
+      position: 'top',
+      title: '註冊失敗！',
+      timer: 1000,
+      icon: 'error',
+      showConfirmButton: false,
+    });
+  };
+
   return (
     <AuthContainer>
       <div>
@@ -47,7 +91,7 @@ const SignUpPage = () => {
           onChange={(passwordInputValue) => setPassword(passwordInputValue)}
         />
       </AuthInputContainer>
-      <AuthButton>註冊</AuthButton>
+      <AuthButton onClick={handleClick}>註冊</AuthButton>
       <Link to="/login">
         <AuthLinkText>取消</AuthLinkText>
       </Link>
