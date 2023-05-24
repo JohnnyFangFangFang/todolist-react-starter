e4ef636065decd3e65e634d1c7222e03c1f7617e
JohnnyFangFangFang <johnny781222@gmail.com>
2023-05-24 13:54:46 +0800

    Add default auth context, install json web token, add auth function and check permission to context

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,102 @@
+import { login, register, checkPermission } from '../api/auth';
+import { createContext, useState, useEffect } from 'react';
+import * as jwt from 'jsonwebtoken';
+import { useLocation } from 'react-router-dom';
+
+const defaultAuthContext = {
+  // 使用者是否登入的判斷依據，預設為 false，若取得後端有效憑證則切換為 true
+  isAuthenticated: false,
+  currentMember: null, // 為當前使用者相關資料，預設為 null，成功登入後就會有使用者資料
+  register: null, // 註冊方法
+  login: null, // 登入方法
+  logout: null, // 登出方法
+};
+
+const AuthContext = createContext(defaultAuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [payload, setPayload] = useState(null);
+  const { pathname } = useLocation();
+
+  // 驗證 token
+  useEffect(() => {
+    const checkTokenIsValid = async () => {
+      const authToken = localStorage.getItem('authToken');
+      // 如果 token 不存在則進行相關設定
+      if (!authToken) {
+        setIsAuthenticated(false);
+        setPayload(null);
+        return;
+      }
+      // 若 token 存在則驗證其有效性
+      const result = await checkPermission(authToken);
+      // 有效
+      if (result) {
+        setIsAuthenticated(true);
+        const tempPayload = jwt.decode(authToken);
+        setPayload(tempPayload);
+      } else {
+        // 無效
+        setIsAuthenticated(false);
+        setPayload(null);
+      }
+    };
+    checkTokenIsValid();
+  }, [pathname]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        currentMember: payload && {
+          id: payload.sub,
+          name: payload.name,
+        },
+        // 註冊
+        register: async (data) => {
+          const { success, authToken } = await register({
+            username: data.username,
+            email: data.email,
+            password: data.password,
+          });
+          const tempPayload = jwt.decode(authToken);
+          if (tempPayload) {
+            setPayload(tempPayload);
+            setIsAuthenticated(true);
+            localStorage.setItem('authToken', authToken);
+          } else {
+            setPayload(null);
+            setIsAuthenticated(false);
+          }
+          return success;
+        },
+        // 登入
+        login: async (data) => {
+          const { success, authToken } = await login({
+            username: data.username,
+            password: data.password,
+          });
+          const tempPayload = jwt.decode(authToken);
+          if (tempPayload) {
+            setPayload(tempPayload);
+            setIsAuthenticated(true);
+            localStorage.setItem('authToken', authToken);
+          } else {
+            setPayload(null);
+            setIsAuthenticated(false);
+          }
+          return success;
+        },
+        // 登出
+        logout: () => {
+          localStorage.removeItem('authToken');
+          setPayload(null);
+          setIsAuthenticated(false);
+        },
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
