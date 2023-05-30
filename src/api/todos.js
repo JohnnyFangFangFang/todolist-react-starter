fa9a58631a1b887a77f6368c19483ab9bb63ee56
JohnnyFangFangFang <johnny781222@gmail.com>
2023-05-30 10:25:45 +0800

    Add axios interceptors

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,13 +1,33 @@
 // axios 設定檔
 
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001';
+const baseUrl = 'https://todo-list.alphacamp.io/api';
+
+// 新增一個 instance，使用方法可見 axios interceptor readme
+const axiosInstance = axios.create({
+  baseURL: baseUrl,
+});
+
+// 在axiosInstance 使用 interceptors 方法
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.error(error);
+  },
+);
 
 // 瀏覽 todos
 export const getTodos = async () => {
   try {
-    const res = await axios.get(`${baseUrl}/todos`);
-    return res.data;
+    const res = await axiosInstance.get(`${baseUrl}/todos`);
+    // 這邊要注意回傳內容，要有兩層 data 才抓得到
+    return res.data.data;
   } catch (error) {
     console.error('[Get Todos failed]: ', error);
   }
@@ -16,8 +36,9 @@ export const getTodos = async () => {
 // 新增 todo
 export const createTodo = async (payload) => {
   const { title, isDone } = payload;
+
   try {
-    const res = await axios.post(`${baseUrl}/todos`, {
+    const res = await axiosInstance.post(`${baseUrl}/todos`, {
       title,
       isDone,
     });
@@ -31,7 +52,7 @@ export const createTodo = async (payload) => {
 export const patchTodo = async (payload) => {
   const { id, title, isDone } = payload;
   try {
-    const res = await axios.patch(`${baseUrl}/todos/${id}`, {
+    const res = await axiosInstance.patch(`${baseUrl}/todos/${id}`, {
       title,
       isDone,
     });
@@ -44,7 +65,7 @@ export const patchTodo = async (payload) => {
 // 刪除 todo
 export const deleteTodo = async (id) => {
   try {
-    const res = await axios.delete(`${baseUrl}/todos/${id}`);
+    const res = await axiosInstance.delete(`${baseUrl}/todos/${id}`);
     return res.data;
   } catch (error) {
     console.error('[Delete Todo failed]: ', error);
